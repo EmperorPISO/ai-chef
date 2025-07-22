b95036319d88b54f73ff89037e193276cb3c3ff3
EmperorPISO <princeokechukwu83@gmail.com>
2025-07-22 16:20:38 +0100

    updated CORS for deploy

--- ai-chef-backend/main.py
+++ ai-chef-backend/main.py
@@ -20,7 +20,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://ai-chef-piso.vercel.app",
-        "http://localhost:5173",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
