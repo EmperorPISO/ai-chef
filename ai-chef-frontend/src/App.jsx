6a6d571d755e8065176c1003e130a9335c04c6be
EmperorPISO <princeokechukwu83@gmail.com>
2025-07-22 17:13:00 +0100

    fixed footer not sticking to the bottom

--- ai-chef-frontend/src/App.jsx
+++ ai-chef-frontend/src/App.jsx
@@ -6,13 +6,15 @@ import Footer from '@/components/Footer'
 
 export default function App() {
 return(
-  <div className='flex-col flex-centered w-full min-h-screen'>
-    <div className='flex-grow max-w-3xl px-1 rounded-md bg-[#F2F3F2] min-h-[90vh] shadow-lg shadow-[#2d882d30]'>
+  <div className='flex-col flex-centered  min-h-screen'>
+    <div>
+      <div className=' flex-grow max-w-3xl px-1 w-full rounded-md bg-[#F2F3F2] min-h-[90vh] shadow-lg shadow-[#2d882d30]'>
       <Header />
       <MainComponent />
-      <Footer />
     </div>
-    
+    <Footer />
+    </div>
+  
   </div>
 )
 }
--- ai-chef-frontend/src/components/Footer.jsx
+++ ai-chef-frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
    const URL = "https://www.linkedin.com/in/prince-okechukwu-30b46a2b9"
     return(
-        <footer className="flex flex-col sm:flex-row items-center justify-center gap-2 h-auto sm:h-14 text-[#550000] bg-[#cbe8cb] min-text px-4 py-2">
+        <footer className=" flex flex-col sm:flex-row items-center justify-center gap-2 h-auto sm:h-14 text-[#550000] bg-[#cbe8cb] min-text px-4 py-2 rounded-b-md">
             <p className="text-center">&copy; 2025 PISO-CODES. All rights reserved. |</p>
             <p>Connect with me on <a 
             href={URL}
